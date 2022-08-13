46ead140d6b6edb62c3a6985aba9222db25c73c1
avillacobh <avillacobh@unal.edu.co>
2022-08-13 15:19:55 -0500

    algunos cambios

--- constants.f90
+++ constants.f90
@@ -2,8 +2,8 @@ module constants
     implicit none
     real(8),parameter :: m = 1.2185/2.0
     real(8),parameter :: hbar = 0.1973269804
-    real(8), parameter :: rmax = 1000
-    real(8), parameter :: dr = 0.01
+    real(8), parameter :: rmax = 10000
+    real(8), parameter :: dr = 0.001
     real(8), parameter ::alpha = 0.29 
     real(8), parameter ::sigma = 1.306
     integer :: nsteps = idint(rmax/dr)
